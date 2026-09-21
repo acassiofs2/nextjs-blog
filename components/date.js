0db94da6909137df0846cc5b2206dc628b3a21f8
Acassio Silva <acassio.silva@ti.hubfloripa.com.br>
2026-09-21 19:57:52 -0300

    chapter 35

---
+++ components/date.js
@@ -0,0 +1,12 @@
+import { parseISO, format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+ 
+export default function Date({ dateString }) {
+  const date = parseISO(dateString);
+  return <time dateTime={dateString}>{format(date, "d 'de' MMMM 'de' yyyy", { locale: ptBR })}</time>;
+}
+
+export function DateNumber({ dateString }) {
+  const date = parseISO(dateString);
+  return <time dateTime={dateString}>{format(date, "dd/MM/yyyy")}</time>;
+}
